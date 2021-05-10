e0af345dc6bd199f29e0c818c4a1de403a9cfba5
Web-Qwelcer <74631643+Web-Qwelcer@users.noreply.github.com>
2021-05-10 12:48:41 +0300

    JS

---
+++ main.js
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////// Завдання 1 ////////////////////////////////////////////////////////////
+
+// function qwelcer(x, y)
+// {
+//     if (x < y)
+//     {
+//         return x;
+//     }
+//     else
+//     {
+//         return y;
+//     }
+// }
+
+// let x = prompt("Enter first number -> ");
+// let y = prompt("Enter second number -> ");
+
+// let res = qwelcer(x, y);
+
+// console.log("More number: " + res);
+
+//////////////////////////////////////////////////////////// Завдання 2 ////////////////////////////////////////////////////////////
+
+// function qwelcer(x, y)
+// {
+//     return x ** y;
+// }
+
+// let x = prompt("Enter number -> ");
+// let y = prompt("Enter degree -> ");
+
+// let res = qwelcer(x, y);
+
+// console.log(res);
