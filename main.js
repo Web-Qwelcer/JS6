7484f07aad3b095c21587bdaa028edb1ee99f729
Web-Qwelcer <74631643+Web-Qwelcer@users.noreply.github.com>
2021-06-17 13:13:15 +0300

    JS

--- main.js
+++ main.js
@@ -32,3 +32,49 @@
 // let res = qwelcer(x, y);
 
 // console.log(res);
+
+//////////////////////////////////////////////////////////// Завдання 3!!! ////////////////////////////////////////////////////////////
+
+// function calculator(fNum, sNum, action) {
+//   switch (action) {
+//     case '+':
+//       res = fNum + sNum;
+//       break;
+//     case '-':
+//       res = fNum - sNum;
+//       break;
+//     case '*':
+//       res = fNum * sNum;
+//       break;
+//     case '/':
+//       res = fNum / sNum;
+//       break;
+//     default:
+//       console.log("Error!!!");
+//   }
+
+//   return res;
+// }
+
+// let fNum = +prompt("Enter first number -> "),
+//   action = prompt("Enter action -> "),
+//   sNum = +prompt("Enter second number -> ");
+
+// let result = calculator(fNum, sNum, action);
+
+// console.log(`${fNum} ${action} ${sNum} = ${result}`);
+
+//////////////////////////////////////////////////////////// Завдання 5!!! ////////////////////////////////////////////////////////////
+
+// function multiplicationTable(num) {
+//   for (let i = 2; i <= 9; i++) {
+//     res = num * i;
+//     console.log(`${num} * ${i} = ${res}`);
+//   }
+
+//   return res;
+// }
+
+// let num = +prompt("Enter number -> ");
+
+// multiplicationTable(num);
